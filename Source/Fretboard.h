575132ba4ab708f1071b03cda1e55491f9718293
cemolcay <ccemolcay@gmail.com>
2017-04-09 19:10:20 +0300

    Shared library setup
    Base setup for iOS, tvOS and macOS shared library

---
+++ Source/Fretboard.h
@@ -0,0 +1,19 @@
+//
+//  Fretboard iOS.h
+//  Fretboard iOS
+//
+//  Created by Cem Olcay on 09/04/2017.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Fretboard.
+FOUNDATION_EXPORT double FretboardVersionNumber;
+
+//! Project version string for Fretboard.
+FOUNDATION_EXPORT const unsigned char Fretboard_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Fretboard/PublicHeader.h>
+
+
---
+++ Source/Fretboard.swift
@@ -0,0 +1,25 @@
+//
+//  Fretboard.swift
+//  Fretboard
+//
+//  Created by Cem Olcay on 09/04/2017.
+//
+//
+
+#if os(iOS) || os(tvOS)
+  import UIKit
+#elseif os(OSX)
+  import AppKit
+#endif
+
+#if os(iOS) || os(tvOS)
+  typealias FRView = UIView
+  typealias FRColor = UIColor
+  typealias FRFont = UIFont
+  typealias FRBezierPath = UIBezierPath
+#elseif os(OSX)
+  typealias FRView = NSView
+  typealias FRColor = NSColor
+  typealias FRFont = NSFont
+  typealias FRBezierPath = NSBezierPath
+#endif
